2c204af2f6eb3d5c9c7b5b7821fdf5ab6ddefcd9
Whyaziz <rizkyaziz214@gmail.com>
2024-06-11 21:37:58 +0700

    feat: pagination

--- src/components/ideas/Contents.jsx
+++ src/components/ideas/Contents.jsx
@@ -5,7 +5,7 @@ import axios from "axios";
 
 export default function Contents() {
   const [ideas, setIdeas] = useState([]);
-  const [sortedIdeas, setSortedIdeas] = useState("published_at");
+  const [sortedIdeas, setSortedIdeas] = useState("-published_at");
   const [appendedIdeas, setAppendedIdeas] = useState("medium_image");
   const [pageSizes, setPageSizes] = useState(8);
   const [page, setPage] = useState(1);
@@ -13,34 +13,38 @@ export default function Contents() {
   const [totalPages, setTotalPages] = useState(0);
   const [totalItems, setTotalItems] = useState(0);
 
-  const allIdeas = async () => {
-    try {
-      const response = await axios.get(
-        "https://suitmedia-backend.suitdev.com/api/ideas"
-      );
-      setTotalItems(response.data.data.length);
-      setTotalPages(Math.ceil(response.data.data.length / pageSizes));
-    } catch (error) {
-      console.error("Error fetching ideas:", error);
-    }
-  };
+  useEffect(() => {
+    const fetchTotalItems = async () => {
+      try {
+        const response = await axios.get(
+          "https://suitmedia-backend.suitdev.com/api/ideas"
+        );
+        setTotalItems(response.data.data.length);
+        setTotalPages(Math.ceil(response.data.data.length / pageSizes));
+      } catch (error) {
+        console.error("Error fetching total items:", error);
+      }
+    };
 
-  const fetchIdeas = async () => {
-    try {
-      const response = await axios.get(
-        `https://suitmedia-backend.suitdev.com/api/ideas?page[number]=${page}&page[size]=${pageSizes}&append[]=${appendedIdeas}&sort=${sortedIdeas}`
-      );
-      setIdeas(response.data.data);
-      console.log("Ideas fetched:", response.data.data);
-    } catch (error) {
-      console.error("Error fetching ideas:", error);
-    }
-  };
+    fetchTotalItems();
+  }, []);
 
   useEffect(() => {
+    const fetchIdeas = async () => {
+      try {
+        const response = await axios.get(
+          `https://suitmedia-backend.suitdev.com/api/ideas?page[number]=${page}&page[size]=${pageSizes}&append[]=${appendedIdeas}&sort=${sortedIdeas}`
+        );
+        setIdeas(response.data.data);
+        console.log("Ideas fetched:", response.data.data);
+      } catch (error) {
+        console.error("Error fetching ideas:", error);
+      }
+    };
+
     fetchIdeas();
-    allIdeas();
-  }, [pageSizes, sortedIdeas, page]);
+    setTotalPages(Math.ceil(totalItems / pageSizes));
+  }, [page, pageSizes, sortedIdeas]);
 
   const handlePreviousPage = () => {
     if (page > 1) {
@@ -54,25 +58,38 @@ export default function Contents() {
     }
   };
 
+  const handleFirstPage = () => {
+    setPage(1);
+  };
+
+  const handleLastPage = () => {
+    setPage(totalPages);
+  };
+
+  const startItem =
+    page === totalPages ? totalItems - pageSizes : (page - 1) * pageSizes + 1;
+  const endItem = Math.min(page * pageSizes, totalItems);
+
   return (
     <div className="flex flex-col w-full relative overflow-x-clip">
-      <div className="bg-white h-[25vh] flex w-[140%] absolute top-[-10%] lg:top-[-10%] left-[-10%] -rotate-3" />
+      <div className="bg-white h-[25vh] flex w-[140%] absolute top-[-5rem] lg:top-[-5rem] left-[-10%] -rotate-3" />
       <div className="bg-white min-h-screen flex flex-col justify-center items-center text-black p-8 lg:p-16 z-10">
-        <div className="flex flex-row w-full justify-between">
+        <div className="flex flex-col lg:flex-row w-full justify-between">
           <div className="w-full flex">
             <p>
-              Showing : {page === 1 ? 1 : pageSizes * (page - 1)} -
-              {pageSizes * page >= totalItems ? totalItems : pageSizes * page}{" "}
-              of {totalItems}
+              Showing: {startItem} - {endItem} of {totalItems}
             </p>
           </div>
           <div className="w-full flex gap-4 justify-end py-4">
             <div className="flex flex-row gap-4 items-center">
-              <p>Show per page : </p>
+              <p>Show per page: </p>
               <select
                 className="p-2 border border-gray-300 rounded-lg"
                 value={pageSizes}
-                onChange={(e) => setPageSizes(parseInt(e.target.value))}
+                onChange={(e) => {
+                  setPageSizes(parseInt(e.target.value));
+                  setPage(1);
+                }}
               >
                 <option value={8}>8</option>
                 <option value={16}>16</option>
@@ -80,14 +97,14 @@ export default function Contents() {
               </select>
             </div>
             <div className="flex flex-row gap-4 items-center">
-              <p>Sort by :</p>
+              <p>Sort by:</p>
               <select
                 className="p-2 border border-gray-300 rounded-lg"
                 value={sortedIdeas}
                 onChange={(e) => setSortedIdeas(e.target.value)}
               >
-                <option value="published_at">Newest</option>
-                <option value="-published_at">Oldest</option>
+                <option value="-published_at">Newest</option>
+                <option value="published_at">Oldest</option>
               </select>
             </div>
           </div>
@@ -98,15 +115,19 @@ export default function Contents() {
               key={idea.id}
               className="flex flex-col w-full relative overflow-clip rounded-lg lg:rounded-xl shadow-xl"
             >
-              <img
-                src="https://placehold.co/600x400/png"
+              <Image
+                src={
+                  //   idea.medium_image[0].url ||
+                  "https://placehold.co/600x400/png"
+                }
                 width={1000}
                 height={1000}
                 className="h-auto w-full object-cover hover:scale-105 transition-transform duration-500 ease-in-out"
+                alt={idea.title}
               />
               <div className="flex flex-col flex-wrap p-4">
                 <p className="text-xs lg:text-sm text-slate-400">
-                  {idea.published_at}
+                  {new Date(idea.published_at).toLocaleDateString()}
                 </p>
                 <p className="line-clamp-3 text-sm lg:text-base hover:text-orange-suit-500 hover:scale-[101%] transition-color duration-150">
                   {idea.title}
@@ -118,20 +139,42 @@ export default function Contents() {
         <div className="flex w-full justify-center items-center mt-4">
           <button
             className="p-2 m-2 border border-gray-300 rounded-lg"
+            onClick={handleFirstPage}
+            disabled={page === 1}
+          >
+            {"<<"}
+          </button>
+          <button
+            className="p-2 m-2 border border-gray-300 rounded-lg"
             onClick={handlePreviousPage}
             disabled={page === 1}
           >
-            Previous
+            {"<"}
           </button>
-          <p>
-            Page {page} of {totalPages}
-          </p>
+          {[...Array(totalPages)].map((_, index) => (
+            <button
+              key={index}
+              className={`p-2 m-2 border border-gray-300 rounded-lg ${
+                page === index + 1 ? "bg-orange-500 text-white" : ""
+              }`}
+              onClick={() => setPage(index + 1)}
+            >
+              {index + 1}
+            </button>
+          ))}
           <button
             className="p-2 m-2 border border-gray-300 rounded-lg"
             onClick={handleNextPage}
             disabled={page === totalPages}
           >
-            Next
+            {">"}
+          </button>
+          <button
+            className="p-2 m-2 border border-gray-300 rounded-lg"
+            onClick={handleLastPage}
+            disabled={page === totalPages}
+          >
+            {">>"}
           </button>
         </div>
       </div>
